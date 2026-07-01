fab6de71fc771467baa7050558c67de30e009523
moorthiivs <moorthi@iviewsense.com>
2026-07-01 21:18:47 +0530

    app icon added

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.irms.app',
-  appName: 'irms-frontend',
+  appName: 'IRMS',
   webDir: 'dist'
 };
 
